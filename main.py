954611089eb0502fe9128bfddde2e8058b988df8
Haochen <haochyu@umich.edu>
2022-08-04 00:06:38 -0400

    Add video frame to QT

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import cv2
 import numpy as np
+import threading
 
-
-def main():
-    cam = cv2.VideoCapture(2)
+def view():
+    cam = cv2.VideoCapture(0)
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
     while True:
         check, frame = cam.read()
@@ -17,10 +17,17 @@ def main():
         for (x, y, w, h) in faces:
             cv2.rectangle(gray, (x, y), (x + w, y + h), (120), 2)
         # Display the output
-        cv2.imshow('img', gray)
+        cv2.imshow('img', frame)
         key = cv2.waitKey(1)
         if key == 27:
             break
+
+def main():
+
+    x = threading.Thread(target=view)
+    x.start()
+    print('hello')
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
